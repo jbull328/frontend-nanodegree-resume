2944451b2083bd56da8077495afaf338240f841e
John T. Bull <jackbull328@gmail.com>
2016-05-04 21:20:21 -0700

    added name and role to resume builder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+  var name = "John Bull";
+  var role = "Front-End Web Developer";
+
+  var formattedName = HTMLheaderName.replace("%data%", name);
+  var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+  $("#header").prepend(formattedRole);
+  $("#header").prepend(formattedName);
